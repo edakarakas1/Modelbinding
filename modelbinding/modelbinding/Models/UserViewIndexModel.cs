f6dbae702ce90fbd99cf6c842f704356b579010e
edakarakas1 <72891447+edakarakas1@users.noreply.github.com>
2022-02-04 09:11:46 +0300

    model

---
+++ modelbinding/modelbinding/Models/UserViewIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace modelbinding.Models
+{
+    public class UserViewIndexModel
+    {
+        public List<usertablo> Users { get; set; }
+
+    }
+}
